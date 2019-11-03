222bd447a62018e87f6202f33dfce97997c4f93d
evs062009 <evs062009@gmail.com>
2019-11-03 10:41:40 +0200

    create maven project

--- SSA/ElementTasks/src/task3TrianglesSorting/domains/Triangle.java
+++ SSA/ElementTasks/src/task3TrianglesSorting/domains/Triangle.java
@@ -7,17 +7,19 @@ import java.math.RoundingMode;
 
 public class Triangle extends Shape{
 
-    public Triangle(String name, double[] parameters) {
-        super(name, parameters);
-    }
-
     public Triangle(ShapeData data) {
         super(data.getString(), data.getDoubles());
+        setResultValue(getHeronTriangleArea(data.getDoubles()[0], data.getDoubles()[1], data.getDoubles()[2]));
     }
 
     @Override
     public String toString() {
         return String.format("[%s]: %s cm", getName(),
-                MathUtilities.round(getValue(), 2, RoundingMode.HALF_UP));
+                MathUtilities.round(getResultValue(), 2, RoundingMode.HALF_UP));
+    }
+
+    private double getHeronTriangleArea(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
